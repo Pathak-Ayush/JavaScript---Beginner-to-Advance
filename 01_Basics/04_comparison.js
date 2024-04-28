6a2108a3760c8e36d40dbc7ac4cf7a1034694878
PathakAyush <ayushpathak11techs@gmail.com>
2024-04-28 14:32:51 +0530

    some other confusing conversions.

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -5,9 +5,15 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
 
+//data types should be same while comparison and 
+
+console.log("2" > 1);
+//string is automatically converted into number.
+console.log("02" > 1);
+
+
+//following conversions are confusing and should generally avoided.
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
@@ -16,6 +22,6 @@ console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// === (strictly check the value)
 
 console.log("2" === 2);
