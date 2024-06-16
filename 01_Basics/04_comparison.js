c9b2621c73062bc15da869e8dade8b46cb49e783
PathakAyush <ayushpathak11techs@gmail.com>
2024-06-16 19:11:53 +0530

    stack and heap memory

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -25,3 +25,31 @@ console.log(undefined < 0);
 // === (strictly check the value)
 
 console.log("2" === 2);
+
+//******************Memory in javascript */
+
+//stack (primitive types) and heap (non-primitive)
+// In stack get a value of decared variable or object
+// In heap memory we get reference value for original variable or object, so any changes done will be to original var,
+
+// stack (any changes will be done in copied value)
+let myName = "Zor se bolo, Germany!" 
+
+let anotherName = "Khaali 7 ghante ka to raasta hai." 
+anotherName = "Khaali 7 ghnate ka raasta? Petrol kya tera baap bharega?"
+
+console.log(myName);
+console.log(anotherName)
+
+//object  heap memory (reference  = any changes done will be in original value)
+
+let user1 = {
+    email: "johndoe@google.com",
+    password: "iqwwdkh32e3"
+}
+
+let user2 = user1
+user2.email = "abc@google.com"
+
+console.log(user1.email);
+console.log(user2.email);
